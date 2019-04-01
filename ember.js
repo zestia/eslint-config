d8381e2723aba5c5d1c9fb01f2881251a935b0e8
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-04-01 11:58:46 +0100

    add new rules

--- ember.js
+++ ember.js
@@ -18,10 +18,15 @@ module.exports = {
   rules: {
     'hbs/check-hbs-template-literals': 'error',
     'ember/no-jquery': 'error',
+    'ember/no-test-and-then': 'error',
+    'ember/no-test-import-export': 'error',
+    'ember/no-unnecessary-index-route': 'error',
+    'ember/no-unnecessary-service-injection-argument': 'error',
     'ember/order-in-components': 'error',
     'ember/order-in-controllers': 'error',
     'ember/order-in-models': 'error',
     'ember/order-in-routes': 'error',
+    'ember/route-path-style': 'error',
     'no-restricted-imports': ['error', {
       paths: [{
         name: '@ember/object/computed',
