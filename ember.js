7b75568a98b37c25c2d0c9c9d25d2c613b829cd3
Andrew Kirwin <andrew.kirwin@zestia.com>
2019-09-02 09:44:29 +0100

    upgrade deps / remove rules that are now part of ember recommended

--- ember.js
+++ ember.js
@@ -17,22 +17,16 @@ module.exports = {
     browser: true
   },
   rules: {
-    'ember/no-ember-super-in-es-classes': 'error',
-    'ember/no-get-properties': 'error',
     'ember/no-get': 'error',
-    'ember/no-invalid-debug-function-arguments': 'error',
     'ember/no-jquery': 'error',
-    'ember/no-new-mixins': 'error',
     'ember/no-test-and-then': 'error',
     'ember/no-test-import-export': 'error',
     'ember/no-unnecessary-index-route': 'error',
-    'ember/no-unnecessary-route-path-option': 'error',
     'ember/no-unnecessary-service-injection-argument': 'error',
     'ember/order-in-components': 'error',
     'ember/order-in-controllers': 'error',
     'ember/order-in-models': 'error',
     'ember/order-in-routes': 'error',
-    'ember/require-return-from-computed': 'error',
     'ember/route-path-style': 'error',
     'hbs/check-hbs-template-literals': 'error',
     // 'ember/use-ember-get-and-set': 'error'
